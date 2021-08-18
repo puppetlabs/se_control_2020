a4cf5b1f43e340d597d4e78b2df530b61e089742
Administrator <admin@example.com>
2021-08-18 20:16:05 +0000

    more bigger better

--- site-modules/example/manifests/windows_regkeys.pp
+++ site-modules/example/manifests/windows_regkeys.pp
@@ -8,7 +8,7 @@ class example::windows_regkeys {
 
   registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value1':
     type => string,
-    data => 'CD4PE and NOW demo test data',
+    data => 'CD4PE and NOW demo better data',
   }
 
   registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value2':
